ada7da5b7571a460564455a2fb429c9c75a117b6
julianmedwards <julian.michael.edwards@gmail.com>
2023-10-12 11:13:54 -0500

    Personal SVGs, fix/config built file pathing.

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
 import './src/index.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+import jeLogoSvg from './src/je-logo-svg'
 import { setupCounter } from './src/counter.js'
 
 const appWrapper = document.querySelector('#app')
 
 appWrapper.classList.add(
-    'm-0',
     'min-w-full',
-    'p-8',
-    'text-center',
-    'bg-zinc-900',
-    'text-white',
+    'min-h-screen',
     'flex',
-    'place-items-center',
-    'min-h-screen'
+    'text-center',
+    'justify-center',
+    'm-0'
 )
 appWrapper.innerHTML = /*html*/ `
-  <div class="min-w-full">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}"  class="inline h-24 p-6" alt="Vite logo"/>
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" class="">
-      <img src="${javascriptLogo}"  class="inline h-24 p-6" alt="JavaScript logo" />
-    </a>
-    <h1 class="text-5xl">Hello Vite!</h1>
-    <div class="p-8">
-      <button id="counter" type="button" class="border-zinc-950 py-2 px-5 border-2 rounded-lg"></button>
-    </div>
-    <p class="text-gray-500">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
+  <main class="min-w-full">
+    <section id="welcome">
+      <svg class="inline h-20 p-4 text-red-700">
+        ${jeLogoSvg}
+      </svg>
+      <h1 class="text-5xl font-medium">Hello!</h1>
+      <div class="p-8">
+        <button id="counter" type="button" class="border-zinc-950 py-2 px-5 border-2 rounded-lg"></button>
+      </div>
+      <p class="text-gray-500">
+        Click on the Vite logo to learn more
+      </p>
+    </section>
+    <section id="projects">
+    </section>
+    <section id="personal">
+    </section>
+    <section id="footer/links">
+    </section>
+  </main>
 `
 
 setupCounter(document.querySelector('#counter'))

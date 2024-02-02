1bb32d89989b6edda4c16373d277a3fdf065f5c9
julianmedwards <julian.michael.edwards@gmail.com>
2024-02-02 14:50:07 -0600

    Add actual books; fix modal scroll positioning

--- src/readingListHtml.js
+++ src/readingListHtml.js
@@ -1,14 +1,47 @@
+const books = [
+    {
+        title: 'The Mythical Man Month',
+        image: 'mythical-man-month-1.jpg',
+        readDate: 'November 2023',
+    },
+    {
+        title: 'The Pragmatic Programmer',
+        image: 'pragmatic-programmer-1.jpg',
+        readDate: 'December 2023',
+    },
+    {
+        title: 'Refactoring',
+        image: 'refactoring-1.jpg',
+        readDate: 'January 2023',
+    },
+    {
+        title: 'The Design of Everday Things',
+        image: 'the-design-of-everday-things-1.jpg',
+        readDate: 'In Progress',
+    },
+]
+
 export default /*html*/ `
-    <div id="reading-list" class="flex justify-center items-center h-dvh w-dvh overflow-scroll">
+    <div id="reading-list" class="h-dvh w-dvh overflow-y-scroll">
         <div id="scrollbar">
             <div id="scrollbar-dot"></div>
         </div>
-        <div>
-            <h1 class="text-3xl">Hello World</h1>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
-            <div class="book"></div>
+        <h1 class="text-3xl">My Reading List</h1>
+        <div class="flex flex-col items-center">
+            ${books.map((book) => buildBookImage(book)).join('')}
         </div>
     </div>
 `
+
+function buildBookImage(book) {
+    return /*html*/ `
+        <img class="book" src="img/books/${book.image}" />
+    `
+}
+
+function buildBookDetails(book) {
+    return /*html*/ `
+        <h1>${book.title}</h1>
+        <h2>${book.readDate}</h2>
+    `
+}
